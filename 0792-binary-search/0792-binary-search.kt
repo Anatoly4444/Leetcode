1f9fc7e3beeee108cce1eb1ad18b7d7567cf4cdb
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-14 13:54:13 +0400

    Time: 245 ms (49.67%), Space: 38.8 MB (77.15%) - LeetHub

--- 0792-binary-search/0792-binary-search.kt
+++ 0792-binary-search/0792-binary-search.kt
@@ -1,8 +1,17 @@
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
-        nums.forEachIndexed{i,v ->
-            if(v == target)
-                return i 
+        var left = 0
+        var right = nums.size - 1
+
+        while(right >= left) {
+            val mid = (left + right) / 2
+            if(nums[mid] == target)
+                return mid
+            else if(nums[mid] > target) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }    
         }
         return -1
     }
