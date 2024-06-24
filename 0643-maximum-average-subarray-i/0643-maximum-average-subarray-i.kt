bf5317deadb5820494f4c595746542a5965143ea
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-24 20:35:53 +0400

    Time: 458 ms (64.87%), Space: 51.3 MB (92.4%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.kt
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.kt
@@ -3,13 +3,14 @@ class Solution {
         val queue = LinkedList<Int>()
         var avg = Double.NEGATIVE_INFINITY
         var sum = 0
+        var x = 0
         for(i in nums) {
             sum += i
             queue.addLast(i)
             if(queue.size > k) {
                 sum -= queue.first
                 queue.removeFirst()
-            }
+            } 
             if(queue.size == k) {
                 val avgTemp = sum.toDouble() / k
                 if(avgTemp > avg)
