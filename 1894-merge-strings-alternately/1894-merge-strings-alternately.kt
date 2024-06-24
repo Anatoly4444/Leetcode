c2caa40f5ce484f08bde565ccaa79f35fc4c33ae
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-24 19:15:21 +0400

    Time: 180 ms (13.24%), Space: 35.5 MB (50.72%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val length = if(word1.length > word2.length) word1.length else word2.length
+        val builder = StringBuilder()
+        for(i in 0..<length) { 
+            val w1 = word1.getOrNull(i)
+            if(w1 != null)
+                builder.append(w1)
+            val w2 = word2.getOrNull(i)
+            if(w2 != null)
+                builder.append(w2)
+        }
+        return builder.toString()
+    }
+}
