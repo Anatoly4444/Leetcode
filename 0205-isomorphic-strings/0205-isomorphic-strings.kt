dc8110bed135332b6ec76a5c396ba6f6392e57e6
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-26 13:34:27 +0400

    Time: 178 ms (80.44%), Space: 36.9 MB (75.21%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.kt
+++ 0205-isomorphic-strings/0205-isomorphic-strings.kt
@@ -1,8 +1,11 @@
 class Solution {
     fun isIsomorphic(s: String, t: String): Boolean {
-        val map = s.indices.associateBy({ s[it] }, { t[it] })  
-        val map2 = t.indices.associateBy({ t[it] }, { s[it] })
-        
+        val map = HashMap<Char, Char>()
+        val map2 = HashMap<Char, Char>()
+        for (i in s.indices) {
+            map.put(s[i], t[i])
+            map2.put(t[i], s[i])
+        }
         for(i in s.indices) {
             if(map.get(s[i]) != t[i] || map2.get(t[i]) != s[i])
                 return false
