1e620a2cd4c3d582c57726e767a734c6e6404457
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-26 13:52:19 +0400

    Time: 260 ms (19.45%), Space: 41.2 MB (5.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.kt
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -1,5 +1,7 @@
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
+        if(s.length != t.length)
+            return false
         return t.toList().sorted() == s.toList().sorted()
     }
 }
