11d193dc1eded07dabf883c631bcb0547b855c03
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-26 14:05:21 +0400

    Time: 211 ms (47.19%), Space: 37.2 MB (72.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.kt
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -1,17 +1,17 @@
 class Solution {
-fun isAnagram(s: String, t: String): Boolean {
-    if(s.length != t.length)
-        return false
-    val map = mutableMapOf<Char, Int>()
-    val map2 = mutableMapOf<Char, Int>()
-    for(i in s.indices) {
-        map.compute(s[i]) { _,v -> if(v == null) 1 else v + 1}
-        map2.compute(t[i]) { _,v -> if(v == null) 1 else v + 1}
-    }
-    for(i in map.keys) {
-        if(map.get(i) != map2.get(i))
+    fun isAnagram(s: String, t: String): Boolean {
+        if(s.length != t.length)
             return false
+        val map = mutableMapOf<Char, Int>()
+        val map2 = mutableMapOf<Char, Int>()
+        for(i in s.indices) {
+            map.compute(s[i]) { _,v -> if(v == null) 1 else v + 1}
+            map2.compute(t[i]) { _,v -> if(v == null) 1 else v + 1}
+        }
+        for(i in map) {
+            if(i.value != map2.get(i.key))
+                return false
+        }
+        return true
     }
-    return true
-}
 }
