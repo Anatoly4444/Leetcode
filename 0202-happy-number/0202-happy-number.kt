c183d2bdf4af27cdf21bd977a858dedfc4ad7050
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-26 17:10:22 +0400

    Time: 184 ms (10.59%), Space: 39.7 MB (5.61%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.kt
@@ -0,0 +1,13 @@
+class Solution {
+    val set = mutableSetOf<Int>()
+    fun isHappy(n: Int): Boolean {
+        if(n in set) 
+            return false 
+        else set.add(n)
+        val list = n.toString().map{ it.digitToInt() }
+        val sum = list.map { it * it }.sum()
+        if(sum == 1)
+            return true
+        return isHappy(sum)
+    }
+}
