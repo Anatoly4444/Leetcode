5afcff0476edc239e65ad64502d94b99e3ab4a57
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-30 19:41:54 +0400

    Time: 166 ms (16%), Space: 34.6 MB (88%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.kt
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.kt
@@ -1,16 +1,16 @@
 class Solution {
     fun sumZero(n: Int): IntArray {
-        var list = IntArray(n)
+        var arr = IntArray(n)
         var counter = 0
         val x: Int = if(n % 2 == 0) {n / 2} else {
-            list[counter] = 0
+            arr[counter] = 0
             counter++
             (n - 1) / 2
         }
         for(i in 1..x) {
-            list[counter++] = i
-            list[counter++] = i * -1
+            arr[counter++] = i
+            arr[counter++] = i * -1
         }
-        return list
+        return arr
     }
 }
