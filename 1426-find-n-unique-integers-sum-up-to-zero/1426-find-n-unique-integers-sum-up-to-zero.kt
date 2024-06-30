8d883a0ef68516495c019b0587680f24e8f9f002
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-06-30 19:20:42 +0400

    Time: 183 ms (8%), Space: 37.4 MB (44%) - LeetHub

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun sumZero(n: Int): IntArray {
+        var list = mutableListOf<Int>()
+        val x: Int = if(n % 2 == 0) {n / 2} else {
+            list.add(0)
+            (n - 1) / 2
+        }
+        for(i in 0..< x) {
+            while(true) {
+                val k = (1..1000).random()
+                if(!list.contains(k)) {
+                    list.add(k)
+                    list.add(k * -1)
+                    break;
+                }
+            }
+        }
+        return list.toIntArray()
+    }
+}
