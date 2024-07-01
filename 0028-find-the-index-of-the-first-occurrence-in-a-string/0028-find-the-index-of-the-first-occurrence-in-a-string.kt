801dcbcc5fba34d6b24103f61ec5d4a8c9ddcd6d
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-07-01 22:24:25 +0400

    Time: 148 ms (47.27%), Space: 35.3 MB (10.8%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun strStr(haystack: String, needle: String): Int {
+        if(!haystack.contains(needle))
+            return -1
+        if(haystack == needle)
+            return 0
+        for(i in haystack.indices) {
+            val symbol = haystack[i]
+            if(needle.startsWith(symbol)) {
+                val endIndex = i + needle.length
+                if(endIndex > haystack.length)
+                    return -1
+                val substring = haystack.substring(i, endIndex)
+                if(needle == substring)
+                    return i
+            }
+        }
+        return -1
+    }
+}
