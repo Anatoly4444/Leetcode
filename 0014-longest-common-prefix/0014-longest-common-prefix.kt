a29bd7dbbbf72df7d48f637a8bfcbb0ce6fdfc2c
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-07-04 14:59:10 +0400

    Time: 223 ms (5.1%), Space: 39 MB (5.18%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.kt
+++ 0014-longest-common-prefix/0014-longest-common-prefix.kt
@@ -1,17 +1,17 @@
 class Solution {
-fun longestCommonPrefix(strs: Array<String>): String {
-    val minLength = strs.asSequence().map(String::length).min()
-    if(minLength == 0)
-        return ""
-    val list = ArrayList<Char>(minLength)
-    for(i in 0..< minLength) {
-        val e1 = strs.get(0).get(i)
-        val allMatch = strs.asSequence().all { it.get(i) == e1 }
-        if(allMatch)
-            list.add(e1)
-        else
-            break
+    fun longestCommonPrefix(strs: Array<String>): String {
+        val minLength = strs.minOf { it.length }
+        if(minLength == 0)
+            return ""
+        val list = ArrayList<Char>(minLength)
+        for(i in 0..< minLength) {
+            val e1 = strs.get(0).get(i)
+            val allMatch = strs.asSequence().all { it.get(i) == e1 }
+            if(allMatch)
+                list.add(e1)
+            else
+                break
+        }
+        return list.joinToString("")
     }
-    return list.joinToString("")
-}
 }
