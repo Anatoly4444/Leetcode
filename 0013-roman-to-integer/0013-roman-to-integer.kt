f54905b3638a5e8674cb87febfa15d6c85bef4fd
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-07-04 13:43:20 +0400

    Time: 243 ms (25.57%), Space: 39.7 MB (6.01%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.kt
+++ 0013-roman-to-integer/0013-roman-to-integer.kt
@@ -21,31 +21,33 @@ fun romanToInt(s: String): Int {
         val i1 = arr[x]
         if(x != arr.lastIndex) {
             val i2 = arr[x + 1]
-            if(i1 == 'I' && i2 == 'V') {
-               sum += 4
-               skipNext = true
+            when {
+                i1 == 'I' && i2 == 'V' ->  { 
+                    sum += 4 
+                    skipNext = true 
+                }
+                i1 == 'I' && i2 == 'X' -> { 
+                    sum += 9
+                    skipNext = true
+                }
+                i1 == 'X' && i2 == 'L' -> { 
+                    sum += 40
+                    skipNext = true
+                }
+                i1 == 'X' && i2 == 'C' -> {
+                    sum += 90
+                    skipNext = true
+                }
+                i1 == 'C' && i2 == 'D' -> {
+                    sum += 400
+                    skipNext = true
+                }
+                i1 == 'C' && i2 == 'M' -> {
+                    sum += 900
+                    skipNext = true
+                }
+                else -> sum += map[i1]!!
             }
-            else if(i1 == 'I' && i2 == 'X') {
-               sum += 9
-               skipNext = true
-            }
-            else if(i1 == 'X' && i2 == 'L') {
-               sum += 40
-               skipNext = true
-            }
-            else if(i1 == 'X' && i2 == 'C') {
-               sum += 90
-               skipNext = true
-            }
-            else if(i1 == 'C' && i2 == 'D') {
-               sum += 400
-               skipNext = true
-            }
-            else if(i1 == 'C' && i2 == 'M'){
-               sum += 900
-               skipNext = true
-            } else 
-                sum += map[i1]!!
         } else {
             sum += map[i1]!!
         }
