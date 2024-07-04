1ecae411af450d9b26cefd44cb73a7bec9cefa39
autkin <87312445+Anatoly4444@users.noreply.github.com>
2024-07-04 19:13:41 +0400

    Time: 195 ms (18.15%), Space: 37.3 MB (11.08%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.kt
@@ -0,0 +1,29 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    val set = mutableSetOf<Int>()
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+        var element = head
+        val zero = ListNode(0)
+        var node = zero
+        while(element != null) {
+            val added = set.add(element.`val`)
+            if(added) {
+                node.next = element
+                node = element
+            } else {
+                if(element.next == null)
+                    node.next = null
+            }
+            element = element.next
+        }
+        return zero.next
+    }
+}
